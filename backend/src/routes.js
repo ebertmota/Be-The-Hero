9ef76c78370a95f7ad403ae5f8877cc702b4214e
ebertmota <ebert.mota03@gmail.com>
2020-03-24 22:17:26 -0300

    building aplication backend

---
+++ backend/src/routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+
+const OngController = require('./controllers/OngController');
+const IncidentsController = require('./controllers/IncidentController');
+const SessionController = require('./controllers/SessionController');
+
+
+const routes = express.Router();
+
+// Session
+routes.post('/sessions', SessionController.store);
+
+
+// Ongs
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.store)
+
+// Incidents
+routes.get('/incidents', IncidentsController.index);
+routes.post('/incidents', IncidentsController.store);
+routes.delete('/incidents/:id', IncidentsController.delete);
+
+module.exports = routes;
