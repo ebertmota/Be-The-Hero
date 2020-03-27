4dc9e65d4db6e225e506d711616f0637bed7c09a
ebertmota <ebert.mota03@gmail.com>
2020-03-27 12:17:02 -0300

    using validators middlewares in routes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
-
+//Controllers
 const OngController = require('./controllers/OngController');
 const IncidentsController = require('./controllers/IncidentController');
 const SessionController = require('./controllers/SessionController');
 const ProfileController = require('./controllers/ProfileController');
+
+//Validators
+const checkAuthorization = require('./validators/authorization');
+const checkBody = require('./validators/ongs');
+const { idValidator, pageValidator } = require('./validators/incidents');
 
 const routes = express.Router();
 
@@ -13,13 +19,23 @@ routes.post('/sessions', SessionController.store);
 
 
 // Ongs
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', checkAuthorization, ProfileController.index);
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.store)
+routes.post('/ongs', checkBody, OngController.store)
 
 // Incidents
-routes.get('/incidents', IncidentsController.index);
-routes.post('/incidents', IncidentsController.store);
-routes.delete('/incidents/:id', IncidentsController.delete);
+routes.get(
+  '/incidents', 
+  checkAuthorization,
+  pageValidator,
+  IncidentsController.index);
+
+routes.post('/incidents', checkAuthorization,IncidentsController.store);
+
+routes.delete(
+  '/incidents/:id', 
+  checkAuthorization, 
+  idValidator,
+  IncidentsController.delete);
 
 module.exports = routes;
