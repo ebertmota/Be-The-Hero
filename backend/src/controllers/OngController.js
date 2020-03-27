50cca365768e895c076300260908c8bf633023c9
ebertmota <ebert.mota03@gmail.com>
2020-03-27 12:23:32 -0300

    checking if ONG already exists before create a new one

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -10,6 +10,16 @@ module.exports = {
 
   async store(req, res) {
     const { name, email, whatsapp, city, uf } = req.body;
+
+    const checkOngExists = await connection('ongs').where({
+      name: name,
+      email: email,
+      whatsapp: whatsapp,
+    }).select('*');
+
+    if (checkOngExists) {
+      return res.status(400).json({ error: 'Ong already exists' })
+    }
     
     const id = crypto.randomBytes(4).toString('HEX');
     
